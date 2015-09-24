08c9e6527f946dba0eb0ac5f6d6cd4ac87b2af8a
Sandeep Singh <ssingh3@walmartlabs.com>
2015-09-24 09:54:12 -0700

    * Clojure project.

---
+++ test/programmingchallenges/core_test.clj
@@ -0,0 +1,7 @@
+(ns programmingchallenges.core-test
+  (:require [clojure.test :refer :all]
+            [programmingchallenges.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
